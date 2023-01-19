e634be7d2d4b7e6bad311f88c4e430238ca69770
Edmilson Soares <esbnet@gmail.com>
2023-01-19 11:19:58 -0300

    feat: :sparkles: create relations on schema

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
+import { appRoutes } from './routes';
 
 const app = Fastify()
-const prisma = new PrismaClient();
 
 app.register(cors, {
   origin: ['http://localhost:3000']
 })
+app.register(appRoutes)
 
-app.get('/habits', async () => {
-  const habits = await prisma.habit.findMany();
-  return habits
-
-})
 
 app.listen({ port: 3333 }).then(() => { console.log('HTTP Server is running!') })
