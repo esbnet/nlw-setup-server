034816bfbeff9f006bbb72a5db9250e5a46cfa2d
Edmilson Soares <esbnet@gmail.com>
2023-01-20 15:41:57 -0300

    feat: :sparkles: create route summary days

---
+++ db.session.sql
@@ -0,0 +1,10 @@
+SELECT
+        D.id,
+        D.date,
+        (
+          SELECT
+            count(*)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed
+      FROM days D
